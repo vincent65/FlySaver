a277dba45092b62f82521003c9aa1dcd63973c96
Vincent Yip <realsharkguy@gmail.com>
2023-11-12 21:20:32 -0500

    search form

--- price_tracking/App.js
+++ price_tracking/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import SearchForm from './src/components/SearchForm';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <SearchForm />
       
+      <SafeAreaView>
+        <SearchForm />
+      </SafeAreaView>
+
       <StatusBar style="auto" />
     </View>
   );
--- price_tracking/src/components/SearchForm.js
+++ price_tracking/src/components/SearchForm.js
@@ -1,15 +1,63 @@
-import {Text, View, StyleSheet} from 'react-native'
+//can also use pressable for buttons
+import {Text, View, StyleSheet, TextInput, Button, Pressable} from 'react-native'
+import { useState } from 'react';
 
 export default function SearchForm() {
-    return( 
-        <View style = {styles.card}>
-            
-        </View>
-    )
+
+    //use state varoable will re render component when the variable changes; smart variable that makes application "reactive"
+    const [from, setFrom] = useState('');
+    const [to, setTo] = useState('');
+
+   
+    const onSearchPress = () => {
+        console.log('Searching for: ', from);
+    };
+
+    return (
+      <View style={styles.card}>
+        <Text style={styles.title}>
+          {" "}
+          Search the best flight prices for your next trip!
+        </Text>
+
+        <TextInput value={from} onChangeText={setFrom} placeholder="From" styles={styles.input} />
+        <TextInput value={to} onChangeText={setTo} placeholder="To" styles={styles.input} />
+
+        <Button title="Search" onPress={onSearchPress}/>
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
     card: {
-        backgroundColor: 'red'
-    }
-})
+        backgroundColor: 'white',
+        margin: 10,
+        padding: 15,
+        borderRadius: 10,
+
+        //shadow
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 4.65,
+
+        elevation: 8,
+    },
+    title: {
+        alignSelf: 'center',
+        fontWeight: '500',
+        fontSize: 16,
+        marginVertical: 15,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: 'gainsboro',
+        padding: 10,
+        marginVertical: 5,
+        borderRadius: 5,
+
+    },  
+});
