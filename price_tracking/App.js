b1a5a5f69e3b4a826f5057df2d51c4555f676bc3
Vincent Yip <realsharkguy@gmail.com>
2023-11-12 22:08:40 -0500

    flight price option list and user interface mostly done

--- price_tracking/App.js
+++ price_tracking/App.js
@@ -1,17 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList} from 'react-native';
 import SearchForm from './src/components/SearchForm';
+import LinearGradient from 'expo-linear-gradient'
+import data from './data';
+import FlightOptionItem from './src/components/FlightPriceOption';
+
+const option1 = data[0];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
+
       <SafeAreaView>
         <SearchForm />
+
+        <FlatList data={data} renderItem={({item})=> <FlightOptionItem flight = {item}/>} showsVerticalScrollIndicator={false}/>
       </SafeAreaView>
 
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
@@ -20,6 +28,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
 });
--- price_tracking/src/components/SearchForm.js
+++ price_tracking/src/components/SearchForm.js
@@ -1,14 +1,18 @@
 //can also use pressable for buttons
 import {Text, View, StyleSheet, TextInput, Button, Pressable} from 'react-native'
 import { useState } from 'react';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import {Feather} from '@expo/vector-icons';
+
 
 export default function SearchForm() {
 
     //use state varoable will re render component when the variable changes; smart variable that makes application "reactive"
     const [from, setFrom] = useState('');
     const [to, setTo] = useState('');
+    const [departDate, setDepart] = useState(new Date());
+    const [returnDate, setReturn] = useState(new Date());
 
-   
     const onSearchPress = () => {
         console.log('Searching for: ', from);
     };
@@ -20,44 +24,80 @@ export default function SearchForm() {
           Search the best flight prices for your next trip!
         </Text>
 
-        <TextInput value={from} onChangeText={setFrom} placeholder="From" styles={styles.input} />
-        <TextInput value={to} onChangeText={setTo} placeholder="To" styles={styles.input} />
+        <TextInput
+          value={from}
+          onChangeText={setFrom}
+          placeholder="From"
+          styles={styles.input}
+        />
+        <TextInput
+          value={to}
+          onChangeText={setTo}
+          placeholder="To"
+          styles={styles.input}
+        />
 
-        <Button title="Search" onPress={onSearchPress}/>
+        <View style={styles.datePicker}>
+
+            <Feather name="calendar" size = {24} color = 'black'/>
+          <DateTimePicker
+            value={departDate}
+            onChange={(event, date) => setDepart(date || newDate)}
+            minimumDate={new Date()}
+          />
+
+          <Text style={{fontSize:25, color: 'gainsboro', marginLeft: 5}}> |</Text>
+          <DateTimePicker
+            value={returnDate}
+            onChange={(event, date) => setReturn(date || newDate)}
+            minimumDate={departDate}
+          />
+        </View>
+
+        <Button title="Search" onPress={onSearchPress} />
       </View>
     );
 }
 
 const styles = StyleSheet.create({
-    card: {
-        backgroundColor: 'white',
-        margin: 10,
-        padding: 15,
-        borderRadius: 10,
+  card: {
+    backgroundColor: "white",
+    margin: 10,
+    padding: 15,
+    borderRadius: 10,
 
-        //shadow
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 4,
-        },
-        shadowOpacity: 0.3,
-        shadowRadius: 4.65,
-
-        elevation: 8,
+    //shadow
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
     },
-    title: {
-        alignSelf: 'center',
-        fontWeight: '500',
-        fontSize: 16,
-        marginVertical: 15,
-    },
-    input: {
-        borderWidth: 1,
-        borderColor: 'gainsboro',
-        padding: 10,
-        marginVertical: 5,
-        borderRadius: 5,
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
 
-    },  
+    elevation: 8,
+  },
+  title: {
+    alignSelf: "center",
+    fontWeight: "500",
+    fontSize: 16,
+    marginVertical: 15,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "gainsboro",
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
+  },
+  datePicker: {
+    borderWidth: 1,
+    borderColor: "gainsboro",
+    padding: 5,
+    marginVertical: 5,
+    borderRadius: 5,
+
+    flexDirection: 'row',
+    alignItems: 'cemter',
+  },
 });
