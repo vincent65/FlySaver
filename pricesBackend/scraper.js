cec9ee5bc83ed291bb2b4a33f1e069fc841da7ee
Vincent Yip <realsharkguy@gmail.com>
2023-11-23 23:24:55 -0500

    finished scraper, goes to correct link then scrapes the price, airline, departure time, arrival time, and duration of flight

--- pricesBackend/scraper.js
+++ pricesBackend/scraper.js
@@ -3,11 +3,63 @@ const puppeteer = require("puppeteer-core");
 const SBR_WS_ENDPOINT =
   "wss://YOUR_BROWSER_USERNAME:YOUR_BROWSER_PASS@brd.superproxy.io:9222";
 
+  async function parseRoute(div) {
+  let airlineSpan = await div.$('[class^="LogoImage_container"] span');
+  let airline = await airlineSpan?.evaluate((el) => el.textContent.trim());
+
+  if (!airline) {
+    let airlineImg = await div.$('[class^="LogoImage_container"] img');
+    airline = await airlineImg?.evaluate((el) => el.alt.trim());
+  }
+
+  const departAt = await div.$eval(
+    '[class^="LegInfo_routePartialDepart"] span',
+    (el) => el.textContent.trim()
+  );
+  const arriveAt = await div.$eval(
+    '[class^="LegInfo_routePartialArrive"] span',
+    (el) => el.textContent.trim()
+  );
+  const duration = await div.$eval(
+    '[class^="LegInfo_stopsContainer"] span',
+    (el) => el.textContent.trim()
+  );
+
+  return {
+    airline,
+    departAt,
+    arriveAt,
+    duration,
+  };
+}
+
+    //returns the prices
+  async function parseFlight(flight) {
+    const price = await flight.$eval(
+      '[class^="Price_mainPriceContainer"]',
+      (el) => el.textContent.trim()
+    );
+    //find all the children of the upperticketbody_legs container to get flight data
+    const [toDiv, fromDiv] = await flight.$$(
+      '[class^="UpperTicketBody_legsContainer"] > div'
+    );
+
+    const to = await parseRoute(toDiv);
+    const from = await parseRoute(fromDiv);
+  return {
+    price,
+  };
+}
+
 async function main() {
   console.log("Connecting to Scraping Browser...");
+
+  const url = `https://www.skyscanner.net/transport/flights/${from}/${to}/${departDate}/${returnDate}/`;
+
   const browser = await puppeteer.connect({
     browserWSEndpoint: SBR_WS_ENDPOINT,
   });
+
   try {
     const page = await browser.newPage();
     console.log("Connected! Navigating to https://example.com...");
@@ -16,8 +68,17 @@ async function main() {
     const client = await page.target().createCDPSession();
     await openDevtools(page, client);
 
-    
-    await page.goto("https://example.com");
+
+    await page.goto(url);
+
+    //close privacy banner
+    await page.locator("#cookieBannerContent button").click();
+
+    //search for the flights; the ^= means starts with
+    const flights = await page.$$('a[class^="FlightsTicket_link"]');
+    //map makes new array with result of parseFlight function on the flights data
+    const data = await Promise.all(flights.map(parseFlight));
+
     console.log("Navigated! Scraping page content...");
     const html = await page.content();
     console.log(html);
